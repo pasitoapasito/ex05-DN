daec1d3a512610ae78a4bfcb7642d04edf726983
DGK <pasito.dev@gmail.com>
2022-11-09 21:12:59 +0900

    feat: 유저 회원가입 기능 구현 (#4)

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls    import path
 
+from config.api     import api
+
 
 """
 Django app url patterns
 """
 urlpatterns = [
+    path("api/", api.urls),
     path('admin/', admin.site.urls),
 ]
