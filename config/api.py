90eff19b3d5d4dcbc93f07c80f7a0750b6bc718e
DGK <pasito.dev@gmail.com>
2022-11-11 22:13:51 +0900

    feat: JWT 인증/인가 기능 구현 (#8)

--- config/api.py
+++ config/api.py
@@ -1,6 +1,10 @@
 from ninja     import NinjaAPI
-from users.api import router as users_router
+
+from users.api         import router as users_router
+from account_books.api import router as account_books_router
 
 
 api = NinjaAPI()
+
 api.add_router('/users', users_router)
+api.add_router('/account-books', account_books_router)
