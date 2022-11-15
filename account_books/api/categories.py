782a260f9c977dfa0e1ab17183ac0fa90243a3d4
DGK <pasito.dev@gmail.com>
2022-11-15 21:36:08 +0900

    feat: 가계부 카테고리 수정/삭제/복구 기능 구현 (#13)

--- account_books/api/categories.py
+++ account_books/api/categories.py
@@ -2,13 +2,14 @@ from ninja import Router
 
 from typing import Optional, List
 
-from django.http      import JsonResponse
+from django.http      import HttpRequest, JsonResponse
 from django.db.models import Q
 
-from core.utils.auth import AuthBearer
-from core.schema     import ErrorMessage
+from core.schema                    import ErrorMessage
+from core.utils.auth                import AuthBearer
+from core.utils.get_obj_n_check_err import GetAccountBookCategory
 
-from account_books.schema import AccountBookCategoryCreateInput, AccountBookCategoryOutput
+from account_books.schema import AccountBookCategoryCreateInput, AccountBookCategoryUpdateInput, AccountBookCategoryOutput
 from account_books.models import AccountBookCategory
 
 
@@ -26,13 +27,13 @@ router = Router()
     auth     = AuthBearer()
 )
 def get_list_account_book_categories(
-    request,
-    search: Optional[str] = None,
-    sort  : str = 'up_to_date',
-    status: str = 'deleted',
-    offset: int = 0,
-    limit : int = 10
-    ):
+    request: HttpRequest,
+    search : Optional[str] = None,
+    sort   : str = 'up_to_date',
+    status : str = 'deleted',
+    offset : int = 0,
+    limit  : int = 10
+    ) -> JsonResponse:
     
     """
     JWT 토큰에서 유저정보 추출
@@ -78,7 +79,10 @@ def get_list_account_book_categories(
     response = {200: AccountBookCategoryOutput, 400: ErrorMessage},
     auth     = AuthBearer()
 )
-def create_account_book_category(request, data: AccountBookCategoryCreateInput):
+def create_account_book_category(
+    request: HttpRequest, 
+    data   : AccountBookCategoryCreateInput
+    ) -> JsonResponse:
     
     """
     JWT 토큰에서 유저정보 추출
@@ -98,3 +102,111 @@ def create_account_book_category(request, data: AccountBookCategoryCreateInput):
                                       name = name
                                   )
     return category
+
+
+"""
+가계부 카테고리 수정 API
+"""
+@router.patch(
+    '/{int:account_book_category_id}',
+    tags     = ['3. 가계부 카테고리'],
+    summary  = '가계부 카테고리 수정',
+    response = {200: AccountBookCategoryOutput, 400: ErrorMessage},
+    auth     = AuthBearer()
+)
+def update_account_book_category(
+    request: HttpRequest, 
+    data   : AccountBookCategoryUpdateInput, 
+    account_book_category_id: int
+    ) -> JsonResponse:
+    
+    """
+    JWT 토큰에서 유저정보 추출
+    """
+    user = request.auth
+
+    """
+    가계부 카테고리 객체/유저정보 확인
+    """
+    category, err = GetAccountBookCategory.get_category_n_check_error(account_book_category_id, user)
+    if err:
+        return JsonResponse({'detail': err}, status=400)
+    
+    if data.name:
+        category.name = data.name
+        
+    category.save()
+
+    return category
+
+
+"""
+가계부 카테고리 삭제 API
+"""
+@router.delete(
+    '/{int:account_book_category_id}',
+    tags     = ['3. 가계부 카테고리'],
+    summary  = '가계부 카테고리 삭제',
+    response = {204: None, 400: ErrorMessage},
+    auth     = AuthBearer()
+)
+def delete_account_book_category(
+    request: HttpRequest, 
+    account_book_category_id: int
+    ) -> JsonResponse:
+    
+    """
+    JWT 토큰에서 유저정보 추출
+    """
+    user = request.auth
+
+    """
+    가계부 카테고리 객체/유저정보 확인
+    """
+    category, err = GetAccountBookCategory.get_category_n_check_error(account_book_category_id, user)
+    if err:
+        return JsonResponse({'detail': err}, status=400)
+    
+    if category.status == 'deleted':
+        return JsonResponse({'detail': f'가계부 카테고리 {account_book_category_id}(id)은/는 이미 삭제된 상태입니다.'}, status=400)
+
+    category.status = 'deleted'
+    category.save()
+
+    return 204, None
+
+
+"""
+가계부 카테고리 복구 API
+"""
+@router.patch(
+    '/{int:account_book_category_id}/restore',
+    tags = ['3. 가계부 카테고리'],
+    summary = '가계부 카테고리 복구',
+    response = {204: None, 400: ErrorMessage},
+    auth = AuthBearer()
+)
+def restore_account_book_category(
+    request: HttpRequest,
+    account_book_category_id: int
+    ) -> JsonResponse:
+    
+    """
+    JWT 토큰에서 유저정보 추출
+    """
+    user = request.auth
+
+    """
+    가계부 카테고리 객체/유저정보 확인
+    """
+    category, err = GetAccountBookCategory.get_category_n_check_error(account_book_category_id, user)
+    if err:
+        return JsonResponse({'detail': err}, status=400)
+    
+    if category.status == 'in_use':
+        return JsonResponse({'detail': f'가계부 카테고리 {account_book_category_id}(id)은/는 이미 복구된 상태입니다.'}, status=400)
+
+    category.status = 'in_use'
+    category.save()
+
+    return 204, None
