66dc378031b3138b7008879c76c6ec46df8ebf8d
DGK <pasito.dev@gmail.com>
2022-11-17 08:55:23 +0900

    feat: 가계부 기록 생성/조회 기능 구현 (#16)

--- account_books/schema.py
+++ account_books/schema.py
@@ -1,4 +1,4 @@
-from typing  import Optional
+from typing  import Optional, List
 from decimal import Decimal
 
 from ninja import Schema
@@ -49,3 +49,86 @@ class AccountBookCategoryOutput(Schema):
         if not obj.user.nickname:
             return None
         return obj.user.nickname
+    
+    
+class AccountBookLogCreateInput(Schema):
+    title: str
+    types: str
+    price: int
+    description: str
+    cateogry_id: int
+    book_id    : int
+    
+    
+class AccountBookLogCreateOutput(Schema):
+    id   : int
+    title: str
+    types: str
+    price: int
+    description: str
+    status     : str
+    book       : str
+    category   : str
+    created_at : str
+    updated_at : str
+    
+    @staticmethod
+    def resolve_book(obj):
+        return obj.book.name
+    
+    @staticmethod
+    def resolve_category(obj):
+        if obj.category.status == 'deleted':
+            category = None
+        else:
+            category = obj.category.name
+        return category
+    
+    @staticmethod
+    def resolve_created_at(obj):
+        return (obj.created_at).strftime('%Y-%m-%d %H:%M')
+    
+    @staticmethod
+    def resolve_updated_at(obj):
+        return (obj.updated_at).strftime('%Y-%m-%d %H:%M')
+    
+    
+class AccountBookLogOutput(Schema):
+    id   : int
+    title: str
+    types: str
+    price: int
+    description: str
+    status     : str
+    book       : str
+    category   : str
+    created_at : str
+    updated_at : str
+    
+    @staticmethod
+    def resolve_book(obj):
+        return obj.book.name
+    
+    @staticmethod
+    def resolve_category(obj):
+        if obj.category.status == 'deleted':
+            category = None
+        else:
+            category = obj.category.name
+        return category
+    
+    @staticmethod
+    def resolve_created_at(obj):
+        return (obj.created_at).strftime('%Y-%m-%d %H:%M')
+    
+    @staticmethod
+    def resolve_updated_at(obj):
+        return (obj.updated_at).strftime('%Y-%m-%d %H:%M')
+    
+    
+class AccountBookLogListOutput(Schema):
+    nickname         : str
+    expected_budget  : int
+    total_income     : Optional[int] = None
+    total_expenditure: Optional[int] = None
+    logs: Optional[List[AccountBookLogOutput]] = None
--- config/api.py
+++ config/api.py
@@ -3,6 +3,7 @@ from ninja import NinjaAPI
 from users.api                    import router as users_router
 from account_books.api.books      import router as account_books_router
 from account_books.api.categories import router as account_book_categories_router
+from account_books.api.logs       import router as account_book_logs_router
 
 
 api = NinjaAPI()
@@ -10,3 +11,4 @@ api = NinjaAPI()
 api.add_router('/users', users_router)
 api.add_router('/account-books', account_books_router)
 api.add_router('/account-books/categories', account_book_categories_router)
+api.add_router('/account-books/logs', account_book_logs_router)
